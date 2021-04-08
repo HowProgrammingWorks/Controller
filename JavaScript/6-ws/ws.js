cac616d6eb1d8e1e90a68ea633970424884d2fb3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-04-08 10:00:40 +0300

    Websocket example

---
+++ JavaScript/6-ws/ws.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { Server } = require('ws');
+
+module.exports = (routing, port) => {
+  const ws = new Server({ port });
+
+  ws.on('connection', (connection, req) => {
+    connection.on('message', async (message) => {
+      const obj = JSON.parse(message);
+      const { name, method, args = [] } = obj;
+      const entity = routing[name];
+      if (!entity) return connection.send('"Not found"');
+      const handler = entity[method];
+      if (!handler) return connection.send('"Not found"');
+      const src = handler.toString();
+      const signature = src.substring(0, src.indexOf(')'));
+      console.log(`${req.socket.remoteAddress} ${name}.${method}`);
+      try {
+        const result = await handler(...args);
+        connection.send(JSON.stringify(result.rows));
+      } catch (err) {
+        console.dir({ err });
+        connection.send('"Server error"');
+      }
+    });
+  });
+
+  console.log(`API on port ${port}`);
+};
