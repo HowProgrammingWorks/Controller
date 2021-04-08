c697a9ed8cd0d9589b2528d4a2c1f7c6cf287cab
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-04-08 10:00:13 +0300

    Fix port argumant

--- JavaScript/5-agnostic/main.js
+++ JavaScript/5-agnostic/main.js
@@ -3,12 +3,10 @@
 const server = require('./http.js');
 const db = require('./db.js');
 
-const PORT = 8000;
-
 const routing = {
   user: require('./user.js'),
   country: db('country'),
   city: db('city'),
 };
 
-server(routing, PORT);
+server(routing, 8000);
